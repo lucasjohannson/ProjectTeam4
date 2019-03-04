b370c0721fc99d3f762f437251f1d07a671ddd1f
lucasjohannson <47370272+lucasjohannson@users.noreply.github.com>
2019-03-04 12:44:57 -0700

    Create Location.java
    idk if we need the setters but put them in anyways

---
+++ Location.java
@@ -0,0 +1,25 @@
+publc clas Location {
+
+private int xCoord;
+private int yCoord;
+
+public int getX () {
+return xCoord;
+}
+
+public int getY () {
+return yCoord;
+}
+
+public void setX (int x) {
+this.xCoord = x;
+}
+
+Public void setY (int y) {
+this.yCoord = y;
+}
+
+
+
+
+}
