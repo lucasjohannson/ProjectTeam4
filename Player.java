bc556b8944783e354197bcf4fdf4d5c10a6d628a
lucasjohannson <47370272+lucasjohannson@users.noreply.github.com>
2019-03-04 12:55:15 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
 public class PLayer{
+  
+  private int health = 100;
+  private ArrayList<> items = new ArrayList<>();
+  private Location location; 
+  private boolean hasItem = false:
+  
+  
+  public Direction getDirection () {
+    
+  }
+  
+  public attackType getMove (enemy:Enemy) {
+    
+  }
+  
+  public boolean hasItem (){
+    if (len.Arraylist<collectible> > 0) {
+      hasItem = true;
+    }
+  }
+    
 
+  
 }
 
 
