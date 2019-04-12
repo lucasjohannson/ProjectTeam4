19827381a6394d2a0c4afb01cb8435903ae5c566
maryannmagnaye <47370282+maryannmagnaye@users.noreply.github.com>
2019-04-12 09:15:56 -0600

    Add files via upload

---
+++ Enemytest.java
@@ -0,0 +1,90 @@
+package teamproj;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class Enemytest {
+	Enemy test1 = new Enemy(null, 0, 0, 0, 0);
+	
+	//TEST ENEMY NAME SETTER
+	@Test
+    public void testsetName() {
+        Enemy card = new Enemy(null, 0, 0, 0, 0);
+        card.setName("test");
+        assertTrue(card.getName() == "test");
+    }
+	
+	//TEST ENEMY NAME GETTER 
+	@Test
+    public void testgetName() {
+        Enemy card = new Enemy(null, 0, 0, 0, 0);
+        card.setName("test");
+        assertTrue(card.getName() == "test");
+    }
+
+	//TEST ENEMY SPEED GETTER
+	@Test
+    public void testgetspeed(){
+        Enemy card =  new Enemy(null, 0, 0, 0, 0);
+        card.setSpeed(2);
+        assertTrue(card.getspeed() == 2);
+    }
+	
+	//TEST ENEMY SPEED SETTER 
+	@Test
+    public void testsetSpeed(){
+        Enemy card = new Enemy(null, 0, 0, 0, 0);
+        card.setSpeed(2);
+        assertTrue(card.getspeed() == 2);
+    }
+     
+
+		//TEST ENEMY POWER GETTER
+		@Test
+	    public void testgetPower(){
+	        Enemy card =  new Enemy(null, 0, 0, 0, 0);
+	        card.setPower(50);
+	        assertTrue(card.getpower() == 50);
+	    }
+		
+		// TEST ENEMY POWER SETTER 
+		@Test
+	    public void testsetPower(){
+	        Enemy card = new Enemy(null, 0, 0, 0, 0);
+	        card.setPower(50);
+	        assertTrue(card.getpower() == 50);
+	    }
+		//TEST ENEMY MAX HEALTH GETTER
+		@Test
+		public void testgetmaxHealth(){
+			Enemy card =  new Enemy(null, 0, 0, 0, 0);
+			card.setmaxCorHealth(50);
+			assertTrue(card.getmaxCorHealth() == 50);
+		}
+				
+		// TEST ENEMY MAX HEALTH SETTER 
+				@Test
+			    public void testsetMaxHealth(){
+			        Enemy card = new Enemy(null, 0, 0, 0, 0);
+			        card.setmaxCorHealth(50);
+			        assertTrue(card.getmaxCorHealth() == 50);
+			    }
+				
+		//TEST ENEMY CURRENT HEALTH GETTER
+		@Test
+		public void testgetCurrentHealth(){
+			Enemy card =  new Enemy(null, 0, 0, 0, 0);
+			card.setcurrentHealth(50);
+			assertTrue(card.getcurrentHealth() == 50);
+			    }
+		
+		//TEST ENEMY CURRENT HEALTH SETTER 
+		@Test
+	    public void testCurrentHealthSetter(){
+	        Enemy card = new Enemy(null, 0, 0, 0, 0);
+	        card.setcurrentHealth(50);
+	        assertTrue(card.getcurrentHealth() == 50);
+	    }
+}
